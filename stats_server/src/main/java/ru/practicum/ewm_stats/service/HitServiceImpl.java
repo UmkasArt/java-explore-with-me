ec9a827583a85d986b3bed6b9020dc1309df93bc
Artemii <artemijpotapov1@yandex.ru>
2023-01-09 16:41:27 +0300

    Diplom: fix - не проходят тесты, исправление v8

--- stats_server/src/main/java/ru/practicum/ewm_stats/service/HitServiceImpl.java
+++ stats_server/src/main/java/ru/practicum/ewm_stats/service/HitServiceImpl.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ru.practicum.ewm_stats.dto.EndpointHit;
-import ru.practicum.ewm_stats.dto.HitMapper;
 import ru.practicum.ewm_stats.dto.ViewStats;
 import ru.practicum.ewm_stats.model.Hit;
 import ru.practicum.ewm_stats.repository.HitRepository;
@@ -15,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static ru.practicum.ewm_stats.dto.HitMapper.toHit;
+import static ru.practicum.ewm_stats.dto.HitMapper.toViewStats;
 
 
 @Service
@@ -47,7 +47,7 @@ public class HitServiceImpl implements HitService {
         } else {
             List<ViewStats> list = new ArrayList<>();
             for (Hit hit : hitRepository.findAllByTimestampBetween(startDate, endDate)) {
-                ViewStats viewStats = HitMapper.toViewStats(hit);
+                ViewStats viewStats = toViewStats(hit);
                 viewStats.setHits(Long.valueOf(countViewsByUri(viewStats.getUri())));
                 list.add(viewStats);
             }
